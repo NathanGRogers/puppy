462a2ae11d3ba6342dd577e571325f3e1236cc49
Nathan Rogers <nrogers.careers@gmail.com>
2023-05-06 22:23:44 -0600

    adding in dog dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/NathanGRogers/puppy
 
 go 1.20
+
+require github.com/NathanGRogers/dog v0.0.0-20230507041632-2655a3812e93 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import (
+	strings
+	"github.com/NathanGRogers/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+
+	func BigBark() string {
+		return dog.WhenGrownUp(Bark())
+
+	}
 }
